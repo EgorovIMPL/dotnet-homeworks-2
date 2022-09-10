ca0443359f0e96d823492b710978b70799eb50fe
EgorovIMPL <egorov.r.684017@gmail.com>
2022-09-10 18:28:59 +0300

    HW1

--- HomeWork1/Hw1/Parser.cs
+++ HomeWork1/Hw1/Parser.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace Hw1;
 
 public static class Parser
@@ -7,13 +9,35 @@ public static class Parser
         out CalculatorOperation operation, 
         out double val2)
     {
-        throw new NotImplementedException();
+        if (IsArgLengthSupported(args))
+        {
+            
+            bool tryVal1 = double.TryParse(args[0], out val1);
+            bool tryVal2 = double.TryParse(args[2], out val2);
+            if (tryVal1 && tryVal2)
+                operation = ParseOperation(args[1]);
+            else
+                throw new ArgumentException();
+        }
+        else
+        {
+            throw new ArgumentException();
+        }
     }
 
     private static bool IsArgLengthSupported(string[] args) => args.Length == 3;
 
     private static CalculatorOperation ParseOperation(string arg)
     {
-        throw new NotImplementedException();
+        if (arg == "+")
+            return CalculatorOperation.Plus;
+        else if (arg == "-")
+            return CalculatorOperation.Minus;
+        else if (arg == "*")
+            return CalculatorOperation.Multiply;
+        else if (arg == "/")
+            return CalculatorOperation.Divide;
+        else
+            throw new InvalidOperationException();
     }
 }
