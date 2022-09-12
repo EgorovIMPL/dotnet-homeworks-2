6e81f2ebbdc0bee3f090418453349e29f17f4be1
EgorovIMPL <egorov.r.684017@gmail.com>
2022-09-12 18:46:44 +0300

    New commit

--- Homework1/Hw1/Parser.cs
+++ Homework1/Hw1/Parser.cs
@@ -1,43 +1,44 @@
-namespace Hw1;
-
-public static class Parser
+namespace Hw1
 {
-    public static void ParseCalcArguments(string[] args, 
-        out double val1, 
-        out CalculatorOperation operation, 
-        out double val2)
+    public static class Parser
     {
-        if (IsArgLengthSupported(args))
+        public static void ParseCalcArguments(string[] args,
+            out double val1,
+            out CalculatorOperation operation,
+            out double val2)
         {
-            var tryVal1 = double.TryParse(args[0], out val1);
-            var tryVal2 = double.TryParse(args[2], out val2);
-            if (tryVal1 && tryVal2)
+            if (IsArgLengthSupported(args))
             {
-                operation = ParseOperation(args[1]);
-                if (operation == CalculatorOperation.Undefined)
-                    throw new InvalidOperationException();
+                var tryVal1 = double.TryParse(args[0], out val1);
+                var tryVal2 = double.TryParse(args[2], out val2);
+                if (tryVal1 && tryVal2)
+                {
+                    operation = ParseOperation(args[1]);
+                    if (operation == CalculatorOperation.Undefined)
+                        throw new InvalidOperationException();
+                }
+                else
+                    throw new ArgumentException();
             }
             else
+            {
                 throw new ArgumentException();
+            }
         }
-        else
+
+        private static bool IsArgLengthSupported(string[] args) => args.Length == 3;
+
+        private static CalculatorOperation ParseOperation(string arg)
         {
-            throw new ArgumentException();
+            CalculatorOperation result = arg switch
+            {
+                "+" => CalculatorOperation.Plus,
+                "-" => CalculatorOperation.Minus,
+                "/" => CalculatorOperation.Divide,
+                "*" => CalculatorOperation.Multiply,
+                _ => CalculatorOperation.Undefined
+            };
+            return result;
         }
-    }
-
-    private static bool IsArgLengthSupported(string[] args) => args.Length == 3;
-
-    private static CalculatorOperation ParseOperation(string arg)
-    {
-        CalculatorOperation result = arg switch
-        {
-            "+" => CalculatorOperation.Plus,
-            "-" => CalculatorOperation.Minus,
-            "/" => CalculatorOperation.Divide,
-            "*" => CalculatorOperation.Multiply,
-            _ => CalculatorOperation.Undefined
-        };
-        return result;
     }
 }
