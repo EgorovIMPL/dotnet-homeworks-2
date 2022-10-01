e2b0308e826a59a77c7d532eacad64f7a5735a20
EgorovIMPL <egorov.r.684017@gmail.com>
2022-10-01 20:44:45 +0300

    HW3

--- Homework3/Hw3.Tests/SingleInitializationSingleton.cs
+++ Homework3/Hw3.Tests/SingleInitializationSingleton.cs
@@ -7,11 +7,14 @@ public class SingleInitializationSingleton
 {
     private static readonly object Locker = new();
 
+    private static SingleInitializationSingleton instance = null;
     private static volatile bool _isInitialized = false;
-
+    
     public const int DefaultDelay = 3_000;
     
     public int Delay { get; }
+    
+    private static int _delayInitialized { get; set; }
 
     private SingleInitializationSingleton(int delay = DefaultDelay)
     {
@@ -22,14 +25,35 @@ public class SingleInitializationSingleton
 
     internal static void Reset()
     {
-        throw new NotImplementedException();
+        instance = null;
+        _isInitialized = false;
     }
 
     public static void Initialize(int delay)
     {
-        throw new NotImplementedException();
+        if (_isInitialized == true)
+            throw new InvalidOperationException();
+        else
+        {
+            _isInitialized = true;
+            _delayInitialized = delay;
+        }
     }
 
-    public static SingleInitializationSingleton Instance => throw new NotImplementedException();
-
+    public static SingleInitializationSingleton Instance
+    {
+        get
+        {
+            if (instance == null)
+                lock (Locker)
+                    if (instance == null)
+                    {
+                        if (_isInitialized == true)
+                            instance = new SingleInitializationSingleton(_delayInitialized);
+                        else
+                            instance = new SingleInitializationSingleton();
+                    }
+            return instance;
+        }
+    }
 }
