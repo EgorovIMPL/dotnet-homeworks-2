9867b159697defc37a25510c116e1e8d12778ba8
EgorovIMPL <egorov.r.684017@gmail.com>
2022-10-30 18:50:37 +0300

    Hw 1-7

---
+++ Homework1/Hw1/Parser.cs
@@ -0,0 +1,44 @@
+namespace Hw1
+{
+    public static class Parser
+    {
+        public static void ParseCalcArguments(string[] args,
+            out double val1,
+            out CalculatorOperation operation,
+            out double val2)
+        {
+            if (IsArgLengthSupported(args))
+            {
+                var tryVal1 = double.TryParse(args[0], out val1);
+                var tryVal2 = double.TryParse(args[2], out val2);
+                if (tryVal1 && tryVal2)
+                {
+                    operation = ParseOperation(args[1]);
+                    if (operation == CalculatorOperation.Undefined)
+                        throw new InvalidOperationException();
+                }
+                else
+                    throw new ArgumentException();
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        private static bool IsArgLengthSupported(string[] args) => args.Length == 3;
+
+        private static CalculatorOperation ParseOperation(string arg)
+        {
+            CalculatorOperation result = arg switch
+            {
+                "+" => CalculatorOperation.Plus,
+                "-" => CalculatorOperation.Minus,
+                "/" => CalculatorOperation.Divide,
+                "*" => CalculatorOperation.Multiply,
+                _ => CalculatorOperation.Undefined
+            };
+            return result;
+        }
+    }
+}
---
+++ Homework4/Hw4/Parser.fs
@@ -0,0 +1,37 @@
+﻿module Hw4.Parser
+
+open System
+open Hw4.Calculator
+
+
+type CalcOptions = {
+    arg1: float
+    arg2: float
+    operation: CalculatorOperation
+}
+
+let isArgLengthSupported (args : string[]) =
+    args.Length = 3
+
+let parseOperation (arg : string) =
+    match arg with
+    | "+" -> CalculatorOperation.Plus
+    | "-" -> CalculatorOperation.Minus
+    | "*" -> CalculatorOperation.Multiply
+    | "/" -> CalculatorOperation.Divide
+    |  _ -> ArgumentException() |> raise
+    
+let parseDouble (str:string) =
+    match Double.TryParse str with
+    | true,double -> Some double
+    | _ -> ArgumentException() |> raise
+    
+let parseCalcArguments(args : string[]) =
+    if args = null || not(isArgLengthSupported args) then ArgumentException() |> raise
+    else
+    {
+         arg1 = (parseDouble args[0]).Value
+         operation = parseOperation args[1]
+         arg2 = (parseDouble args[2]).Value
+    }
+    
