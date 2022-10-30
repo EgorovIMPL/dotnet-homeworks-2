c9b11de1cfa018df6781257635c431273f782301
EgorovIMPL <egorov.r.684017@gmail.com>
2022-10-30 17:17:11 +0300

    Hw 7 done

--- Homework7/Hw7/MyHtmlServices/HtmlHelperExtensions.cs
+++ Homework7/Hw7/MyHtmlServices/HtmlHelperExtensions.cs
@@ -1,3 +1,6 @@
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
@@ -7,6 +10,79 @@ public static class HtmlHelperExtensions
 {
     public static IHtmlContent MyEditorForModel(this IHtmlHelper helper)
     {
-        throw new NotImplementedException();
+        var model = helper.ViewData.Model;
+        var properties = helper.ViewData.ModelMetadata.ModelType.GetProperties();
+        if (model is null)
+            return GetRequest(properties);
+        return PostRequest(properties, model);
+    }
+    
+    private static IHtmlContent PostRequest(PropertyInfo[] properties, object? model)
+    {
+        var builder = new HtmlContentBuilder();
+
+        foreach (var property in properties)
+        {
+            var validationAttributes = property.GetCustomAttributes<ValidationAttribute>();
+            var propertyValue = property.GetValue(model);
+
+            builder.AppendHtmlLine($"<div>{GetForm(property)}");
+            
+            foreach (var validationAttribute in validationAttributes)
+            {
+                if (!validationAttribute.IsValid(propertyValue))
+                {
+                    builder.AppendHtmlLine(
+                        $"{GetLabel($"{property.Name}", string.Empty)}" +
+                        $"<span>{validationAttribute.ErrorMessage}</span>");
+                }
+            }
+            
+            builder.AppendHtmlLine("<br></div>");
+        }
+
+        return builder;
+    }
+
+
+    private static IHtmlContent GetRequest(PropertyInfo[] properties)
+    {
+        var builder = new HtmlContentBuilder();
+
+        foreach (var property in properties)
+            builder.AppendHtmlLine($"<div>{GetForm(property)}<br></div>");
+
+        return builder;
+    }
+
+    private static string GetForm(PropertyInfo property)
+    {
+        var name = property.Name;
+        var type = property.PropertyType;
+        var displayAttribute = property.GetCustomAttribute<DisplayAttribute>();
+        var labelContent = GetLabelContent(property.Name, displayAttribute);
+
+        if (type.IsEnum)
+        {
+            var enumNames = type.GetEnumNames();
+            var selectTag = $"<select>{string.Join("", enumNames.Select(el => $"<option value=\"{el}\">{el}</option>").ToArray())}</select>";;
+            
+            return $"{GetLabel($"{name}", labelContent)}<br>{selectTag}<br>";
+        }
+        
+        var contentType = type == typeof(string) ? "text" : "number";
+
+        return $"{GetLabel($"{name}", labelContent)}<br>" +
+                   $"<input id=\"{name}\" name=\"{name}\" type=\"{contentType}\">";
+    }
+
+    private static string GetLabel(string attribute, string content) => 
+        $"<label for=\"{attribute}\">{content}</label>";
+    
+    private static string GetLabelContent(string propertyName, DisplayAttribute? attribute)
+    {
+        if (attribute is null)
+            return Regex.Replace(propertyName, "([A-Z])", " $1",RegexOptions.Compiled).Trim();
+        return attribute.Name;
     }
 } 
