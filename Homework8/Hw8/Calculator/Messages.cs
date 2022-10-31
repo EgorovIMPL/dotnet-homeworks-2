2dfa60fe10ac027704ee9ef798be35afc1917a74
EgorovIMPL <egorov.r.684017@gmail.com>
2022-10-31 12:36:56 +0300

    Message correction

--- Homework8/Hw8/Calculator/Messages.cs
+++ Homework8/Hw8/Calculator/Messages.cs
@@ -2,7 +2,7 @@ namespace Hw8.Calculator;
 
 public static class Messages
 {
-    public const string DivisionByZeroMessage = "Деление на 0. Результат не определён";
+    public const string DivisionByZeroMessage = "Деление на 0";
     public const string InvalidNumberMessage = "Введите числа";
     public const string InvalidOperationMessage = "Недопустимая операция. Допустимые: plus, minus, multiply и divide";
 }
